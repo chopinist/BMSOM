664cc5f1b54d6fb5069c21aee636c5a84e588105
Eylam Keshet <riokarma@gmail.com>
2014-03-28 11:56:47 +0300

    db

--- db/migrate/20140328084405_heroku.rb
+++ db/migrate/20140328084405_heroku.rb
@@ -1,4 +1,30 @@
 class Heroku < ActiveRecord::Migration
   def change
+    create_table "reservations", force: true do |t|
+      t.integer  "user_id"
+      t.integer  "room_id"
+      t.datetime "time"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+
+    create_table "rooms", force: true do |t|
+      t.string   "name",                                 null: false
+      t.boolean  "contains_grand_piano", default: false, null: false
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+
+    create_table "users", force: true do |t|
+      t.string   "first_name",                        null: false
+      t.string   "last_name",                         null: false
+      t.string   "username",                          null: false
+      t.string   "email"
+      t.string   "password_digest"
+      t.string   "instrument",      default: "Other", null: false
+      t.boolean  "admin",           default: false,   null: false
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
   end
 end
