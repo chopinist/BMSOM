9be4a51fb7a2e4ed56e876cd3c7cc19b4e02c0ac
Eylam Keshet <riokarma@gmail.com>
2014-03-29 10:29:51 +0300

    no sleep

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -16,7 +16,6 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    sleep 1
     @reservation = @user.reservations.build(safe_params)
 
     if !params.has_key?(:room_name)
